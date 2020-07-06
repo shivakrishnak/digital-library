a6aa5f657dee056170028cbb2824f36002c8a984
Shivakrishna Kurremula <shivakrishna_kurremula@epam.com>
2020-07-06 16:15:33 +0530

    added flyway - data migration

---
+++ book-service/src/main/resources/db/migration/V2__data.sql
@@ -0,0 +1,5 @@
+Insert into book_id_generator(sequence_name, next_val) values ('book',0);
+
+Insert into category (name, id) values (100, 'Fiction');
+Insert into category (name, id) values (101, 'Non-Fiction');
+Insert into category (name, id) values (102, 'Software Development');
